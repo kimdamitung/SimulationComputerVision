dc825b4c4a2b0a26e04b182c06a84ba7ba2b62e3
kimdamitung <150353472+kimdamitung@users.noreply.github.com>
2024-02-24 15:27:02 +0700

    Update STT 1 nhan_dang

--- Nhan_dang/STT01.py
+++ Nhan_dang/STT01.py
@@ -3,20 +3,17 @@ import numpy as np
 
 image_path = "D:/Thuc_Hanh/Ky_6_2024/BaiTapThiGiacMayTinh/BaiTapMoPhongNhom/img/input_06.png"
 img = cv2.imread(image_path)
-height, width = img.shape[:2]
-new_height = int(height * 0.9)
-new_width = int(width * 0.9)
-img = cv2.resize(img, (new_width, new_height))
 
 def identified_Circle(img):
-	gray = cv2.cvtColor(img , cv2.COLOR_BGR2GRAY)
-	median_img = cv2.medianBlur(gray, 5)
-	circles = cv2.HoughCircles(median_img, cv2.HOUGH_GRADIENT, 1.011, 20, param1=100, param2=28, minRadius=0, maxRadius=55)
-	circles = np.uint16(np.around(circles))
-	for i in circles[0, :]:
-		cv2.circle(img, (i[0], i[1]), i[2], (0, 69, 255), 5)
-		cv2.putText(img, "circles", (i[0], i[1]), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 69, 255), 1)
-
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	blurred = cv2.blur(gray, (3, 3))
+	detected_circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, 20, param1=60, param2=25, minRadius=20, maxRadius=60)
+	if detected_circles is not None:
+		detected_circles = np.uint16(np.around(detected_circles))
+		for pt in detected_circles[0, :]:
+			a, b, r = pt[0], pt[1], pt[2]
+			cv2.circle(img, (a, b), r, (0, 69, 255), 2)
+			cv2.putText(img, "Circle", (a, b), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 69, 255), 1)
 
 def identified_Triangle(img):
 	# code
@@ -28,11 +25,12 @@ def identified_Triangle(img):
 	    x = approx.ravel()[0]
 	    y = approx.ravel()[1]
 	    if len(approx) == 3:
-	        cv2.drawContours(img, [approx], 0, (0, 69, 255), 5)
+	        cv2.drawContours(img, [approx], 0, (0, 69, 255), 2)
 	        cv2.putText(img, "Triangle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 69, 255), 1)
 
 identified_Circle(img)
 identified_Triangle(img)
+
 cv2.imshow("Example", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
